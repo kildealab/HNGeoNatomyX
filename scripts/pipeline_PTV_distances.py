be61bd0ca9ecd0dc18516910fdf2d00d43e67b8f
OdetteRiosIb <132397199+OdetteRiosIb@users.noreply.github.com>
2025-07-01 02:02:39 -0400

    Update pipeline_PTV_distances.py

--- scripts/pipeline_PTV_distances.py
+++ scripts/pipeline_PTV_distances.py
@@ -10,153 +10,66 @@ import rtdsm
 from time import process_time
 import gc, os
 import helpers
+import pandas as pd
 
+import helpers 
+from helpers import get_path_RS, get_body_keys, sort_body_keys, get_keysall
+from helpers import get_name_files, get_body_keys_not_RS
+from helpers import get_path_RS_CT,  search_cuts_z, get_distances_from_contours
 
-PATH_SRC = '/mnt/iDriveShare/OdetteR/Registration_and_contours/Contours/' # location of RT-struct files
-
+#SET AN STANDARD RESOLUTION IN X AND Y OF THE MEDICAL PROTOCOL
+#IN OUR CENTRE WAS THE FOLLOWING
 IMG_RES = [0.51119071245194, 0.51119071245194, 3]
 RADIUS_FRAC = 0.75
 
-
-def get_equal_body(body2,body1,z_min,z_max,r,h,k):
-    bbody2,bbody1 = trim_contours_to_match_zv3(body1.points,body2.points,z_min,z_max)
-    #bbody2,bbody1 = trim_contours_to_match_z2(body2.points, body1.points)
-    d2 = pv.PolyData(bbody2).connectivity(largest=True)
-
-    c2_3 = get_surface_marching_cubes(d2.points).smooth(n_iter = 0)
-    cc2 = pv.PolyData(c2_3).connectivity(largest=True)
-    
-    d1 = pv.PolyData(bbody1).connectivity(largest=True)
-    c11 = get_surface_marching_cubes(d1.points).smooth(n_iter = 0)
-
-    cc1 = pv.PolyData(c11).connectivity(largest=True)
-    c1 = c11.copy()
-
-    #ax_x = max(c2_3.points[:,0])
-    #in_x = min(c2_3.points[:,0])
-    
-    #h = max_x - ((max_x - (min_x))/2)
-    #k = (max(c2_3.points[:,1]))- r*0.5
-
-    max_z = max(c1.points[:,2])
-    min_z = min(c1.points[:,2])
-    
-    spacing_z = get_contour_z_spacing(c1.points)
-    height = (max_z - min_z)  #+ 3*spacing_z
-   
-    z = np.mean([min_z,max_z])
-    mesh = pv.CylinderStructured(center=[h,k,z], direction=[0,0,1], theta_resolution=50,z_resolution=80,radius=r*0.5, height=height)
-    bounding_cylinder3 = get_surface_marching_cubes(mesh.points).smooth(n_iter = 0)
-    s_body1 = cc1.copy().boolean_intersection(bounding_cylinder3.copy())
-
-    max_z2 = max(c2_3.points[:,2])
-    min_z2 = min(c2_3.points[:,2])
-    spacing_z2 = get_contour_z_spacing(c1.points)
-    height2 = (max_z2 - min_z2) + 2*spacing_z2
-   
-    z2 = np.mean([min_z2,max_z2])
-    
-    bounding_cylinder = Cylinder(center=[h,k,z2], direction=[0,0,1], radius=r*2, height=height2)
-    
-    s_body1.compute_implicit_distance(bounding_cylinder, inplace=True)
-    cloud_trim = s_body1.threshold(0.0, scalars="implicit_distance", invert=True)
-    
-    return cloud_trim,c2_3
-
-
-def get_equal_bodyv2(body2,body1,z_max,z_min,h,k,r):
-    body_2 = pv.PolyData(body2).connectivity(largest=True)
-
-    d1 = pv.PolyData(body1).connectivity(largest=True)
-
-    bx = d1.points[:,0]
-    by = d1.points[:,1]
-    bz = d1.points[:,2]
-
-    indexes = (bx-h)**2+(by-k)**2<=(r*0.8)**2
-
-    bx2 = bx[indexes==True]
-    by2 = by[indexes==True]
-    bz2 = bz[indexes==True]
-
-    points22 = list(zip(bx2,by2,bz2))
-    d11 =pv.PolyData(points22)
-
-    theta = np.linspace(0,2*np.pi,300)
-
-    x = r*0.5 * np.cos( theta ) +h
-    y = r*0.5 * np.sin( theta ) +k
-
-    body_crop = [] #array points (cloud)
-    zs = [i for i in d11.points[:,2]]
-    zss = sorted(list(dict.fromkeys(zs)))
-    for z in zss:
-        ptosxy = []
-        for p in d11.points:
-            if z==p[2]:
-                ptosxy.append([float(p[0]),float(p[1])])
-        #pp1= Polygon(ptosxy)
-        pp1= Polygon(ptosxy).buffer(0)
-        pp2 = Polygon(list(zip(x,y)))
-        pp3 = intersection(pp1,pp2)
-        try:
-            bx,by = pp3.exterior.coords.xy[0],pp3.exterior.coords.xy[1]
-        except:
-            coords = [[len(list(x.exterior.coords)),list(x.exterior.coords)] for x in pp3.geoms]
-            bx,by = np.array((sorted(coords)[-1][-1]))[:,0],np.array((sorted(coords)[-1][-1]))[:,1]
-
-        for j in range(0,len(bx)):
-            body_crop.append((float(bx[j]),float(by[j]),float(z)))
-
-    bbody = pv.PolyData(body_crop)
-    bbody2,bbody1 = trim_contours_to_match_zs(body_2.points, bbody.points,z_min,z_max)
-    s_body22 = get_surface_marching_cubes(bbody2)
-    s_body2 = pv.PolyData(s_body22).connectivity(largest=True)
-
-    s_body11 = get_surface_marching_cubes(bbody1)
-    s_body1 = pv.PolyData(s_body11).connectivity(largest=True)
-    return s_body1,s_body2
-
-
-PATH_DEST = 'Thesis_120/xvalues/'
+PATH_DEST = 'xvalues/'
 if not os.path.isdir(PATH_DEST):
     os.makedirs(PATH_DEST)
     
 ROWS = ['xmin', 'xmax', 'xmed', 'xave', 'xstd']
-
-def pipeline_dist_body(param_name='xvalues',path_contours, path ):
+def pipeline_dist_body(param_name='xvalues',path_contours, CSV_patient_ids,path_CBCTs):
+    t_init = process_time()
+    #CSV_patient_ids = '/mnt/iDriveShare/OdetteR/Registration_and_contours/IDS_News_Partial.csv'
+    ids_patients = []
     
-    file = '/mnt/iDriveShare/OdetteR/Registration_and_contours/IDS_News_Partial.csv'
-    ids_news = []
-    
+    #READ THE CSV IDS FILE
     with open(file, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         for row in spamreader:
-            ids_news.append(row[0])
+            ids_patients.append(str(row[0]))
     
+    #CHECK IF THE PATIENT ALREADY HAS A CSV FILE IN THE DESTINATION FOLDER
     existing_patients = [csv_filename.split('_')[-1].split('.')[0] for csv_filename in os.listdir(PATH_DEST)]
-    t_init = process_time()
-
-    for str_pat_id in ['630']:
-        #print('Processing patient ' + str_pat_id + '.') 
-        patient_path = '/mnt/iDriveShare/OdetteR/Registration_and_contours/Contours/'+str_pat_id
-        
-        
+    
+    for str_pat_id in ids_patients:
+        #e.g. path_contours = '/mnt/iDriveShare/OdetteR/Registration_and_contours'
+        patient_contours_path = path_contours+str_pat_id       
         
         # check if patient already has csv
         if str_pat_id in existing_patients:
             print('Patient already has csv:' + str_pat_id)
             continue
         else:
-            print('Processing patient: ' + str_pat_id)
+            #GETS PROCESSING TIME FOR THE PIPELINE
             t0 = process_time()
-            
+            print('Processing patient: ' + str_pat_id)
+          
             contours = []
             key_bodies_to_save = []
             
-            body_list = [d for d in os.listdir(patient_path) if d[0:4] == 'Body']
+            body_list = [d for d in os.listdir(patient_contours_path) if d[0:4] == 'Body']
             
+            # e.g. path_full_CBCT_id = '/mnt/iDriveShare/Kayla/CBCT_images/kayla_extracted/'+str_pat_id+'/'
+            path_full_CBCT_id = path_CBCTs+str_pat_id+'/'
+
             ptv = []
+
+            #GETS THE NAME FILES OF EACH CT OR CBCT MEDICAL IMAGE
+            CT,CBCTs = get_name_files(path_full_CBCT_id)
+                
+            #SET THE PATH OF THE CT 
+            path_CT = path_full_CBCT_id+CT
+            
             if len(body_list)==0:
                 path_rs = get_path_RS(str_pat_id, path_k)
                 bodies_rs = get_body_keys(path_rs)
@@ -170,108 +83,76 @@ def pipeline_dist_body(param_name='xvalues',path_contours, path ):
                         if body not in key_bodies_to_save:
                             key_bodies_to_save.append(body)
                             contours.append(body_contour)
-                keys_all = get_keysall(str_pat_id,path_rs)
-            
-                ptv_key = [i for i in keys_all if i=='z_PTV_ALL' or i=='z_PTVAll' or i=='z_PTV_All' or i=='PTV_ALL' or i=='PTV_All']
-                
-                contour_ptv = rtdsm.get_pointcloud(ptv_key[0], path_rs, False)[0]
-                ptv.append(contour_ptv)
-                
             else:
-                    
+                #GETS KEYS THAT ARE NOT IN THE RS FILE
                 bodies = get_body_keys_not_RS(body_list)
-                formatt = get_format(body_list)
-        
-                path_k1 = '/mnt/iDriveShare/Kayla/CBCT_images/kayla_extracted/'+str_pat_id+'/'
-                CT,CBCTs = get_name_files(path_k1)
-                path_CT = path_k1+CT
-                path_rs_b0 = get_path_RS_v5(path_CT)
-        
+                
+                #SET THE PATH FOR THE RS FILE FOR THE FRACTION 0 (CT SIM IMAGE)
+                path_rs_b0 = get_path_RS_CT(path_CT)
                 bodies.insert(0,'BODY')
                 
-                for bodx in bodies:
-                    if bodx=='BODY':
+                for bodx in range(0,len(bodies)):
+                    if bodies[bodx]=='BODY':
                         body_contour = rtdsm.get_pointcloud('BODY', path_rs_b0, False)[0]
                         contours.append(body_contour)
-                         
                     else:
-                        for bodi in body_list:
-                             body_in_folder = bodi.split('.')[0]
-                             
-                             if body_in_folder==bodx:
-                                 formatt = bodi.split('.')[-1]
-                        path_RS0 = patient_path+'/'+bodx+'.'+formatt
-                        print(path_RS0)
-                    
+                        body_in_folder = body_list[bodx].split('.')[0]  
+                        format_single_contour = body_list[bodx].split('.')[-1]
+                        path_RS0 = patient_path+'/'+bodies[bodx]+'.'+format_single_contour
 
-                        #path_RS0 = patient_path+'/'+bodx+'.'+formatt
-                        if formatt=='json':
+                        #CHECKS IF THE CONTOUR POINTS ARE IN JSON OR DCM
+                        if format_single_contour=='json':
                             f = open(path_RS0)
                             data = json.load(f)
                             f.close()
-                            body_contour = np.array(data[bodx])
+                            body_contour = np.array(data[bodies[bodx]])
                             contours.append(body_contour)
                         else:
-                            body_contour = rtdsm.get_pointcloud(bodx, path_RS0, False)[0]
+                            body_contour = rtdsm.get_pointcloud(bodies[bodx], path_RS0, False)[0]
                             contours.append(body_contour)
                             
                 key_bodies_to_save = bodies.copy()
-                keys_all = get_keysall(str_pat_id,path_rs_b0)
-            
-                ptv_key = [i for i in keys_all if i=='z_PTV_ALL' or i=='z_PTVAll' or i=='z_PTV_All' or i=='PTV_ALL' or i=='PTV_All']
+              
+            #GETS THE KEYS AVAILABLE IN THE RT STRUCTURE FILE
+            keys_all = get_keysall(str_pat_id,path_rs_CT)
                 
-                contour_ptv = rtdsm.get_pointcloud(ptv_key[0], path_rs_b0, False)[0]
-                ptv.append(contour_ptv)
-                print(ptv_key[0])
-                            
+            #GETS THE PTV KEY. IT MAY HAVE SOME VARIATIONS IN THE LABEL, DEPENDING ON HOW THE TECHNICIAN SAVED IT
+            ptv_key = [i for i in keys_all if i=='z_PTV_ALL' or i=='z_PTVAll' or i=='z_PTV_All' or i=='PTV_ALL' or i=='PTV_All']
+
+            #GET THE CONTOUR AS POINT CLOUD AND KEEP IT FOR LATER
+            contour_ptv = rtdsm.get_pointcloud(ptv_key[0], path_rs_CT, False)[0]
+            ptv.append(contour_ptv)
             
-            # initialize dataframe and define output file name
-            #PARA GUARDAR LOS DATOS, DEFINE LOS NOMBRES. 
-            path_k1 = '/mnt/iDriveShare/Kayla/CBCT_images/kayla_extracted/'+str_pat_id+'/'
-            CT,CBCTs = get_name_files(path_k1)
-            path_CT = path_k1+CT
-            path_rs_b0 = get_path_RS_v5(path_CT)
-            
+            # Initialize dataframe and define output file name
             df = pd.DataFrame({param_name : ROWS})
             out_file = param_name + '_' + str_pat_id + '.csv'
             out_path = os.path.join(PATH_DEST,out_file) 
 
             # ================================================================================
-            # CALCULATE PARAMETERS\
+            # CALCULATE PARAMETERS
         
-            print(key_bodies_to_save)
-
-    
             contours_PTV = ptv[0]
-            #CT_path = get_info_replanned(str_pat_id,0)
-                
-            #start_x2, start_y2, start_z2, pixel_spacing2 = get_start_position_dcm(CT_path)
-            pixel_spacing2 = [IMG_RES[0],IMG_RES[1]] 
+            #SET THE PIXEL SPACING
+            pixel_spacing = [IMG_RES[0],IMG_RES[1]] 
             
             for key_body_n in range(0,len(key_bodies_to_save)):
+                t1 = process_time()
                 contours_body = contours[key_body_n]
                 params = []
-                
-#                try:
-                
-                t1 = process_time()
-                distances = get_distances_from_contours(contours_PTV, contours_body,pixel_spacing2)
+               
+                distances = get_distances_from_contours(contours_PTV, contours_body,pixel_spacing)
                     
                 print('\tProcess time of parameters (' + key_bodies_to_save[key_body_n] + '): ' + str(process_time()-t1) + ' s')
 
                 # RECORD PARAMETERS
                 # keep order same as ROWS!
-                
-
-                
+           
                 params.append(np.min(distances))
                 params.append(np.max(distances))
                 params.append(np.median(distances))
                 params.append(np.mean(distances))
                 params.append(np.std(distances))
                 
-         
-
                 df[key_bodies_to_save[key_body_n]] = params
                 # ================================================================================
                 
@@ -279,10 +160,15 @@ def pipeline_dist_body(param_name='xvalues',path_contours, path ):
             df.to_csv(out_path, index=False)
             print('\t' + param_name + ' printed to csv: ' + out_path)
             print('Elapsed time for patient: ' + str((process_time()-t0)/60) + ' min')
-        #print('DONE! Elapsed time for pipeline: ' + str((process_time()-t_init)/3600) + ' hours')
+        print('DONE! Elapsed time for pipeline: ' + str((process_time()-t_init)/3600) + ' hours')
     
 
 if __name__ == "__main__":
-    pipeline_dist_body()
+    #e.g. paths
+    path_contours = '/mnt/iDriveShare/OdetteR/Registration_and_contours'
+    CSV_patient_ids =  '/mnt/iDriveShare/OdetteR/Registration_and_contours/IDS_News_Partial.csv'
+    path_CBCTs = '/mnt/iDriveShare/Kayla/CBCT_images/kayla_extracted/'
+    
+    pipeline_dist_body(path_contours,CSV_patients_ids,path_CBCTs)
 
 
