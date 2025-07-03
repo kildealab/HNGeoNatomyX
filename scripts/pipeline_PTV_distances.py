90c192c2dc85582db80428cdbcad31d6a0a68c49
OdetteRiosIb <132397199+OdetteRiosIb@users.noreply.github.com>
2025-07-03 01:34:50 -0400

    Update pipeline_PTV_distances.py

--- scripts/pipeline_PTV_distances.py
+++ scripts/pipeline_PTV_distances.py
@@ -1,6 +1,6 @@
 """
 Created on Jul 2023
-@author: James Manalad
+@author: James Manalad & Odette Rios-Ibacache
 
 """
 
@@ -42,7 +42,7 @@ def pipeline_dist_body(param_name='xvalues',path_contours, CSV_patient_ids,path_
     existing_patients = [csv_filename.split('_')[-1].split('.')[0] for csv_filename in os.listdir(PATH_DEST)]
     
     for str_pat_id in ids_patients:
-        #e.g. path_contours = '/mnt/iDriveShare/OdetteR/Registration_and_contours'
+        #e.g. path_contours = '/mnt/iDriveShare/OdetteR/Registration_and_contours/Contours/'
         patient_contours_path = path_contours+str_pat_id       
         
         # check if patient already has csv
