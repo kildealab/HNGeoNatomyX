69f5b6bdabedfc8c83903c7a3393e4655db073e1
OdetteRiosIb <132397199+OdetteRiosIb@users.noreply.github.com>
2025-07-03 17:11:52 -0400

    Update treatment_mask_contouring.py

--- scripts/treatment_mask_contouring.py
+++ scripts/treatment_mask_contouring.py
@@ -4,7 +4,6 @@ Created on January 2024
 
 """
 
-
 from skimage import data
 from skimage.util.dtype import dtype_range
 from skimage.util import img_as_ubyte
@@ -12,51 +11,37 @@ from skimage import exposure
 from skimage.morphology import disk
 from skimage.morphology import ball
 from skimage.filters import rank
-import cv2
-import pydicom
-import os
+import cv2, os, json
 import numpy as np
 from skimage import data
 from skimage.morphology import disk
 from skimage.filters import median
 import pydicom as dcm
-import numpy as np
 import rtdsm
-import cv2
-import json
 from matplotlib import pyplot as plt
 
-from skimage.morphology import erosion, dilation, opening, closing, white_tophat  # noqa
-from skimage.morphology import black_tophat, skeletonize, convex_hull_image  # noqa
+from skimage.morphology import erosion, dilation, opening, closing, white_tophat  
+from skimage.morphology import black_tophat, skeletonize, convex_hull_image 
 from skimage.morphology import disk 
-
 
 def get_slice(CT_path):
     positions = []
     for f in [file for file in os.listdir(CT_path) if 'CT' in file]:
         d = dcm.dcmread(CT_path+'/'+f)
         positions.append(d.ImagePositionPatient) 
-    
     positions = sorted(positions, key=lambda x: x[-1])
- 
     return positions
     
-def get_path_RS(path_k):
-    path_patient = path_k
-   
+def get_path_RS(path_patient):
     file_RS = sorted([x for x in os.listdir(path_patient) if 'RS' in x])[-1]
-    print(file_RS)
-    path2 = os.path.join(path_patient, file_RS)
- 
-    return path2
+    full_path = os.path.join(path_patient, file_RS)
+    return full_path
 
 def get_start_position_dcm(CT_path):
     positions = []
     for f in [file for file in os.listdir(CT_path) if 'CT' in file]:
         d = dcm.dcmread(CT_path+'/'+f)
-        
         positions.append(d.ImagePositionPatient)
-        #print(CT_path+'/'+f) 
     positions = sorted(positions, key=lambda x: x[-1])
     start_z = positions[0][2]
     start_x = positions[0][0]
@@ -65,88 +50,61 @@ def get_start_position_dcm(CT_path):
     
     return start_x, start_y, start_z, pixel_spacing 
 
-def get_info2(path_k):
-    path_patient = path_k
+def get_info_CT(path_patient):
     file_RS = sorted([x for x in os.listdir(path_patient) if x[9:11]=='CT' in x])[0]
-    print(sorted([x for x in os.listdir(path_patient) if x[9:11]=='CT' in x]))
-
-    path2 = os.path.join(path_patient, file_RS)
-    files = [x for x in os.listdir(path2) if 'CT' in x]
-    files2 = []
+    full_path = os.path.join(path_patient, file_RS)
+    return full_path
     
-    return path2
-    
-def contour_mask(slice_im,ct_files,h,k,a,b,r1,r2,thres):
-    j = slice_im
-    ct_file = ct_files[j]
-    img = ct_file.pixel_array
+def get_contour_mask(masked_img,h_center,k_center,min_radius,max_radius,thres_grey):
+    positions = []
 
-    img3 =img.copy()
-    img3[img<thres]=0
-    img3[img>=thres]=1
-    maslcirc = []
-    pos = []
-    values = np.linspace(a,b,200)
-    values2 = np.linspace(r1,r2,200)
-    
-    for t in values:
-
+    values_angles = np.linspace(min_angle,max_angle,200)
+    values_radius = np.linspace(min_radius,max_radius,200)
+ 
+    for angle in values:
         count = 0 
         posx = 0
         posy = 0
-        for r in sorted(values2)[::-1]:
-            x = r*np.cos(t*np.pi/180) + h 
-            y = r*np.sin(t*np.pi/180)+ k 
+        for r in sorted(values_radius)[::-1]:
+            x = r*np.cos(angle*np.pi/180) + h_center
+            y = r*np.sin(angle*np.pi/180)+ k_center
   
-            if img3[int(y)][int(x)]==1:
+            if masked_img[int(y)][int(x)]==1:
                 count = count +1
                 if count==1:
                     posx = int(x)
                     posy = int(y)
-   
-                    pos.append([[posx,posy]])
-    mask2x_m = []
-    for p in pos:
-        mask2x_m.append(p[0][0])
-    mask2y_m = []
-    for p in pos:
-        mask2y_m.append(p[0][1])
+                    positions.append([[posx,posy]])
+                    
+    mask_pointsx = []
+    for point in positions:
+        mask_pointsx.append(point[0][0])
+    mask_pointsy = []
+    for point in positions:
+        mask_pointsy.append(point[0][1])    
     
-   # fx,ax = plt.subplots(1,2,figsize=(10,4))
-   # ax[0].imshow(img3)
-#    ax[0].scatter(mask2x_m,mask2y_m,c='r',s=1)
- #   ax[1].imshow(img)
- #   ax[1].scatter(mask2x_m,mask2y_m,c='r',s=1)
-  #  plt.show()
+    return positions,mask_pointsx,mask_pointsy
     
-    return pos
+def add_coords(positions, ct_path,slice_number):
     
-def add_coords(pos, ct_path,j):
-    
-    mask_m = pos.copy()
+    mask_m = positions.copy()
     start_x, start_y, start_z, spacing,p = get_start_position_dcm(ct_path)
     x,y = get_ROI_pixel_array_m(mask_m,start_x,start_y,spacing)
 
-    z = float(get_slice(ct_path)[j][2])
+    z = float(get_slice(ct_path)[slice_number][2])
     for x1 in range(0,len(x)):
-            #    z = ((j + start_z/3)*3) 
         contour_m.append([x[x1],y[x1],z])
-
-
+    
 def get_mask_nifti(roi_array,start_x,start_y,pixel_spacing):
     x = []
     y = []
   
     for i in range(0,len(roi_array)):
-        #print(roi_array[i])
-        
         x.append(((roi_array[i][0]/pixel_spacing[0]) - (start_x/pixel_spacing[0])))
         y.append(((roi_array[i][1]/pixel_spacing[1]) - (start_y/pixel_spacing[1])))
-        
     return x, y
     
 def get_ROI_pixel_array_m(roi_array,start_x,start_y,pixel_spacing):
- 
     x = []
     y = []
     
@@ -154,162 +112,110 @@ def get_ROI_pixel_array_m(roi_array,start_x,start_y,pixel_spacing):
 
         x.append(((roi_array[i][0][0]) + start_x/pixel_spacing[0])*pixel_spacing[0])
         y.append(((roi_array[i][0][1]) + start_y/pixel_spacing[1])*pixel_spacing[1])
-        
     return x, y
-    
+
+
+def save_mask_contour(contour_mask,patient_id_str):
+    mesh = pv.PolyData(contour_mask)
+    mesh.connectivity(largest=True)
+    mask_json = {'Mask' : contour_mask}
+    with open("registered/mask/Mask_"+patient_id_str+".json", "w") as outfile:
+        json.dump(mask_json, outfile)
+    print('save at '+ "registered/mask/Mask_"+patient_id_str+".json")
+    return
 ######################
 
-PATH = '/mnt/iDriveShare/Kayla/CBCT_images/kayla_extracted/' # Path to patient directories
-patients = os.listdir(PATH)
-files = [f for f in os.listdir(PATH) if os.path.isfile(f)]
-patients_path = [PATH+patient+"/" for patient in patients]
+#path_CBCTs = '/mnt/iDriveShare/Kayla/CBCT_images/kayla_extracted/' # Path to patient directories
+patients = os.listdir(path_CBCTs)
+files = [f for f in os.listdir(path_CBCTs) if os.path.isfile(f)]
+patients_path = [path_CBCTs+patient+"/" for patient in patients]
 
-t = insert('Insert patient number: ')
+patient_number_id_folder = insert('Insert patient number: ')
 
-ct_path2 = get_info2(patients_path[patients.index(t)])
-
+ct_path = get_info_CT(patients_path[patients.index(patient_number_id_folder )])
 ct_files = [pydicom.dcmread(os.path.join(ct_path2, f)) for f in os.listdir(ct_path2) if 'CT' in f]
+path_RS = get_path_RS(ct_path)
 
 # Sort the CT files in ascending order by their image position (z-axis)
 ct_files.sort(key=lambda x: x.ImagePositionPatient[2])
 ct_file = ct_files[0]
 
-
-
-#j = slice_im
-import numpy as np
-import rtdsm
-import cv2
-from matplotlib import pyplot as plt
-
-from skimage.morphology import erosion, dilation, opening, closing, white_tophat  # noqa
-from skimage.morphology import black_tophat, skeletonize, convex_hull_image  # noqa
-from skimage.morphology import disk 
-
-
-h,k,a,b,r1,r2,thres1 = 250,350,-175,-5,55,250,60
-for j in range(20,len(ct_files)):
-    print(j)
+def contour_slice(ct_files,ct_path,slice_value,h_center,k_center,min_radius,max_radius,thres_grey,plot=True):
+    #e.g. h_center,k_center,min_angle,max_angle,min_radius,max_radius,thres_grey = 250,350,-175,-5,55,250,60
+    
+    #MINIMUM SLICE WHERE YOU CAN VISUALIZE THE MASK. THIS VALUE CAN BE CHANGED DEPENDING ON EACH IMAGE.
+    minimum_slice_with_mask= 20
+    #for slice_value in range(minimum_slice_with_mask,len(ct_files)):
+    print('------- CURRENTLY WORKING ON SLICE NUMBER'+ str(j)+'------')
+    
     points_to_use = []
-    ct_file = ct_files[j]
+    ct_file = ct_files[slice_value]
     img = ct_file.pixel_array
-    n_img = cv2.normalize(img, None, alpha = -200, beta = img.max(), norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
-    start_x, start_y, start_z, spacing,positions = get_start_position_dcm(ct_path2)
-    mR= rtdsm.get_pointcloud('BODY', path_RS, False)[0]
-    z = mR[:,2]
-    zs = list(dict.fromkeys(z))
-    #for j in range(0,len(zs)):
-    body_slice = []
-    for p in mR:
-        if p[2]==zs[j]:
-            body_slice.append(p[0:2])
-    
-    px,py = get_mask_nifti(body_slice,start_x,start_y,spacing)
-    slicee = list(zip(px,py))
-    
-    points_to_use.append(slicee)
 
+    if plot=True:
+        plt.imshow(img,camp='gray')
+        plt.title('Slice to be contour including BODY and TREATMENT MASK EDGE')
+        plt.show()
+        
+    normalized_img = cv2.normalize(img, None, alpha = -200, beta = img.max(), norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
+    start_x, start_y, start_z, spacing = get_start_position_dcm(ct_path)
+    body_contour= rtdsm.get_pointcloud('BODY', path_RS, False)[0]
+    z_values_body_contour = body_contour[:,2]
+    zs_sorted = list(dict.fromkeys(z_values_body_contour))
+        
+    body_slice = []
+    for point in body_contour:
+        if point[2]==zs_sorted[slice_value]:
+            body_slice.append(point[0:2])
+    
+    points_x,points_y = get_mask_nifti(body_slice,start_x,start_y,spacing)
+    slicee = list(zip(points_x,points_y))
+    points_to_use.append(slicee)
     ctr = np.array(slicee).reshape((-1,1,2)).astype(np.int32)
-            
-    mask2 = np.zeros((512,512), np.uint8)
-    cv2.fillPoly(mask2,pts=ctr,color=(255,255,255))
-    dilated = dilation(mask2, disk(1))
-    ret, thresh = cv2.threshold(dilated,0, 1, cv2.THRESH_BINARY)
-            
+
+    mask_zeros = np.zeros((512,512), np.uint8)
+    cv2.fillPoly(mask_zeros,pts=ctr,color=(255,255,255))
+    dilated = dilation(mask_zeros, disk(1))
+    ret, thresh = cv2.threshold(dilated,0, 1, cv2.THRESH_BINARY)            
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnt = sorted(contours, key=lambda x: cv2.contourArea(x),reverse=True)
-    mask22 = np.zeros((512,512), np.uint8)
-    cv2.drawContours(mask22, cnt, 0, 255, -1)
-  
-            #cv2.drawContours(mask, cnt, 0, 1, -1)
+    mask_zeros_2 = np.zeros((512,512), np.uint8)
+    cv2.drawContours(mask_zeros_2, cnt, 0, 255, -1)
+
+    img2 = normalized_img.copy()
+    img2[normalized_img<thres_grey] = 0
+    img2[normalized_img>=thres_grey] = 1
     
-    
-    #plt.imshow(mask22)
-    #plt.show()
-        
-    img2 =n_img.copy()
-    img2[n_img<thres1]=0
-    img2[n_img>=thres1]=1
-    
-    
-    #norm_image = cv2.normalize(img3, None, alpha = -1000, beta = img.max(), norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
-    #img_s = show_slice_window(norm_image,50, img.max())
-    
-    #clp = intensity_seg(img_s, 200, 290)
-    #med = median(img_s, disk(5))
-    #print(int(np.max(np.array(points_to_use)[0][:,1])))
-    img22 = dilation(img2, disk(2))
+    img_dilated_2 = dilation(img2, disk(2))
     mask_c = np.zeros(img.shape[:2], dtype="uint8")
+    
+    #THE VALUES 11, AND 20 CAN BE CHANGED DEPENDING ON THE RESULTS. THESE VALUES ARE USED TO REMOVE THE TREATMENT TABLE
     mask_c[0:int(np.max(np.array(points_to_use)[0][:,1]))-11, :] = 255
     
     mask_r = np.zeros(img.shape[:2], dtype="uint8")
     mask_r[0:int(np.max(np.array(points_to_use)[0][:,1]))-11, int(np.min(np.array(points_to_use)[0][:,0]))-20:int(np.max(np.array(points_to_use)[0][:,0]))+20] = 255
     
+    masked_img = cv2.bitwise_and(img_dilated_2,img_dilated_2,mask = mask_c)
     
-    masked_img = cv2.bitwise_and(img22,img22,mask = mask_c)
+    disk_radius_1 = disk(1)
+    img_erosed = erosion(masked_img, disk_radius_1)
+    img_dilated_6 = dilation(img_erosed, disk(6))
+    disk_radius_2 = disk(2)
     
-    footprint = disk(1)
-    img3 = erosion(masked_img, footprint)
-    #med = median(img3, disk(1))
-    img4 = dilation(img3, disk(6))
-    footprint = disk(2)
-    
-    img5 = erosion(img4, footprint)
-    masked_img2 = cv2.bitwise_and(img5,img5,mask = mask_r)
-    
-       
-    maslcirc = []
+    img_erosed_2 = erosion(img_dilated_6, disk_radius_2)
+    masked_img2 = cv2.bitwise_and(img_erosed_2,img_erosed_2,mask = mask_r)
 
-    pos = []
-    values = np.linspace(a,b,200)
-    values2 = np.linspace(r1,r2,200)
- 
-    for t in values:
-        count = 0 
-        posx = 0
-        posy = 0
-        for r in sorted(values2)[::-1]:
-            x = r*np.cos(t*np.pi/180) + h 
-            y = r*np.sin(t*np.pi/180)+ k 
-  
-            if masked_img2[int(y)][int(x)]==1:
-                count = count +1
-                if count==1:
-                    posx = int(x)
-                    posy = int(y)
-   
-                    pos.append([[posx,posy]])
-    mask2x_m = []
-    for p in pos:
-        mask2x_m.append(p[0][0])
-    mask2y_m = []
-    for p in pos:
-        mask2y_m.append(p[0][1])
+    if plot=True:
+        plt.imshow(masked_img2,camp='gray')
+        plt.title('Mask to be contoured including BODY and TREATMENT MASK EDGE')
+        plt.show()
     
+    positions,mask_pointsx,mask_pointsy = get_contour_mask(masked_img2,h_center,k_center,min_radius,max_radius,thres_grey)
+
     mask_t = np.zeros(img.shape[:2], dtype="uint8") 
-    cv2.drawContours(mask_t, np.array(pos), 0, 255, -1)
-    
-    #plt.imshow(masked_img)
-    #plt.show()
-    plt.imshow(masked_img2)
-    #plt.plot(np.array(points_to_use[0][:,0]),np.array(points_to_use)[0][:,1],'ro')
-    plt.scatter(mask2x_m,mask2y_m,c='r',s=1)
-    plt.show()
-    add_coords(pos, ct_path2,j)
-    
-    #fx,ax = plt.subplots(1,2,figsize=(10,4))
-    #ax[0].imshow(img3)
-    #ax[0].scatter(mask2x_m,mask2y_m,c='r',s=1)
-    #ax[1].imshow(img)
-    #ax[1].scatter(mask2x_m,mask2y_m,c='r',s=1)
-    #plt.show()
-    
-#import pyvista as pv
+    cv2.drawContours(mask_t, np.array(positions), 0, 255, -1)
 
-mesh = pv.PolyData(contour_m)
-mesh.connectivity(largest=True)
-pati = '671'
-mask3 = {'Mask' : contour_m}
-with open("registered/mask/Mask_"+pati+".json", "w") as outfile:
-    json.dump(mask3, outfile)
-print('save at '+ "registered/mask/Mask_"+pati+".json")
+add_coords(positions, ct_path2,slice_value)
+
+    
+
