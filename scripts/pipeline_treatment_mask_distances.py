21e9603bc2e74feff7946e591b9e8fccb806b817
OdetteRiosIb <132397199+OdetteRiosIb@users.noreply.github.com>
2025-07-03 15:40:13 -0400

    Update pipeline_treatment_mask_distances.py

--- scripts/pipeline_treatment_mask_distances.py
+++ scripts/pipeline_treatment_mask_distances.py
@@ -155,7 +155,7 @@ def pipeline_mask_distances(param_name='distancesMask',path_contours, CSV_patien
                     else:
 
                         contour_body = contours[key_body]
-                        h,k = get_estimate_center(contour_body,r,0.5)
+                        h,k = get_estimate_center(contour_body,r)
                     
                         trim_body2,trim_mask2 = trim_contours_to_match_zs_edge(contour_body,pv.PolyData(mask).points,z_min,z_max)
                    
