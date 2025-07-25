514baa6cc6406a1f0c9c5cdd32eb27e3330a89c2
OdetteRiosIb <132397199+OdetteRiosIb@users.noreply.github.com>
2025-07-24 23:48:51 -0400

    Update pipeline_distances3D_neck.py

--- scripts/pipeline_distances3D_neck.py
+++ scripts/pipeline_distances3D_neck.py
@@ -100,7 +100,7 @@ def pipeline_distances_neck(param_name='distances3Dneck', path_contours,CSV_pati
                 bodies.insert(0,'BODY')
                 body_list.insert(0,'BODY')
 
-                key_mandible = get_key_mandible(str_pat_id,path_rs_b0)
+                key_mandible = get_key_mandible(path_rs_b0)
                 mandible_contour = rtdsm.get_pointcloud(key_mandible,path_rs_b0,False)[0]
               
                 for bodx in range(0,len(bodies)):
